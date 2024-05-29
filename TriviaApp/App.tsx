40eee8cc1cb774333e5596418a64cf42cb3b5a55
navapon <navapon.prspj@gmail.com>
2024-05-29 21:21:43 +0700

    add code

---
+++ TriviaApp/App.tsx
@@ -0,0 +1,23 @@
+import 'react-native-gesture-handler';
+import * as React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import HomeScreen from './src/screens/HomeScreen';
+import QuizScreen from './src/screens/QuizScreen';
+import LeaderboardScreen from './src/screens/LeaderboardScreen';
+
+const Stack = createStackNavigator();
+
+const App = () => {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Quiz" component={QuizScreen} />
+        <Stack.Screen name="Leaderboard" component={LeaderboardScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
+
+export default App;
---
+++ TriviaApp/README.md
@@ -0,0 +1,37 @@
+# TriviaApp
+
+TriviaApp เป็นแอปพลิเคชันที่ช่วยในการทดสอบความรู้ด้วยการตอบคำถามทางด้านต่าง ๆ โดยมีฟีเจอร์หลัก ๆ ดังนี้:
+
+- แสดงคำถามจำนวน 20 ข้อ ทุกๆ คำถามมีตัวเลือกที่เป็นไปได้ 4 ตัวเลือก
+- ทุกครั้งที่เปิดหรือรีโหลดแอปพลิเคชัน คำถามและตัวเลือกจะถูกสุ่ม
+- แสดง Leaderboard เพื่อแสดงผลคะแนนสูงสุดของผู้เล่น
+
+## Technologies Used
+
+- React Native
+- TypeScript
+
+## Installation
+
+1. Clone the repository:
+
+   ```bash
+   git clone <repository_url>
+
+2. Navigate to the project directory:
+
+   ```bash
+   cd TriviaApp
+
+3. Install dependencies:
+     ```bash
+  npm install
+
+
+## Usage
+
+1. Start the app:
+
+   ```bash
+   npm start
+
